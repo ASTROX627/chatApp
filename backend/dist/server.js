ce183f8725a8bcea0eb6fa368b29601d4a613b4b
astrox627 <ar.noornahal42@gmail.com>
2025-08-29 18:06:43 +0330

    Deployment

--- backend/dist/server.js
+++ backend/dist/server.js
@@ -19,11 +19,12 @@ const i18n_1 = __importDefault(require("./core/i18n"));
 const socket_1 = require("./socket/socket");
 const file_route_1 = __importDefault(require("./routes/file.route"));
 const seen_route_1 = __importDefault(require("./routes/seen.route"));
+const path_1 = __importDefault(require("path"));
 dotenv_1.default.config();
 const PORT = Number(process.env.PORT) || 5000;
 socket_1.app.use(express_1.default.json());
 socket_1.app.use((0, cors_1.default)({
-    origin: process.env.CLIENT_URL,
+    origin: "http://localhost:8000",
     credentials: true
 }));
 socket_1.app.use((0, cookie_parser_1.default)());
@@ -31,6 +32,7 @@ socket_1.app.use(i18next_http_middleware_1.default.handle(i18n_1.default));
 socket_1.app.get('/favicon.ico', (req, res) => {
     res.status(204).send();
 });
+socket_1.app.use(express_1.default.static(path_1.default.join(__dirname, "../../frontend/dist")));
 socket_1.app.use("/api/auth", auth_route_1.default);
 socket_1.app.use("/api/messages", protectRoute_1.default, message_route_1.default);
 socket_1.app.use("/api/users", users_route_1.default);
@@ -38,6 +40,9 @@ socket_1.app.use("/api/group", protectRoute_1.default, group_route_1.default);
 socket_1.app.use("/api/profile", protectRoute_1.default, profile_route_1.default);
 socket_1.app.use("/api/files", profile_route_1.default, file_route_1.default);
 socket_1.app.use("/api/seen", profile_route_1.default, seen_route_1.default);
+socket_1.app.get(/.*/, (req, res) => {
+    res.sendFile(path_1.default.join(__dirname, "../../frontend/dist/index.html"));
+});
 socket_1.server.listen(PORT, '0.0.0.0', () => {
     (0, connectToMongoDB_1.default)();
     console.log(`server running on port ${PORT}`);
