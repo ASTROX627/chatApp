649f98b4a9014957ee48b76ceaff0506e1a2c3a1
astrox627 <ar.noornahal42@gmail.com>
2026-07-12 12:52:52 +0330

    fix bugs

--- backend/dist/utils/detectUrl.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.detectUrl = void 0;
-const detectUrl = (message) => {
-    const urlRegex = /(https?:\/\/[^\s]+)/g;
-    return urlRegex.test(message);
-};
-exports.detectUrl = detectUrl;
